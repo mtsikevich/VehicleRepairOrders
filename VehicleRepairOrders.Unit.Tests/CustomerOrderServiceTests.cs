d423cfe558b6f7d349a0bbba809381cad4b3fe09
Tumelo Motsikelane <tumelo.motsikelane@gmail.com>
2023-01-21 21:02:47 +0200

    Added CustomerOrderServiceTests.cs

---
+++ VehicleRepairOrders.Unit.Tests/CustomerOrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+namespace VehicleRepairOrders.Unit.Tests;
+
+public class CustomerOrderServiceTests
+{
+    [Theory]
+    [ClassData(typeof(RuledInputData))]
+    public void GetOrderStatus_ForInputWithSpecificRules(Input inputEntry, OrderStatus status)
+    {
+        var customerOrderService = new CustomerOrderService();
+        var orderStatus = customerOrderService.GetOrderStatus(inputEntry);
+
+        orderStatus.Should().Be(status);
+    }
+}
+
+class RuledInputData : TheoryData<Input, OrderStatus>
+{
+    public RuledInputData()
+    {
+        Add(new Input{ IsLargeOrder = true, OrderType = OrderType.Repair, IsNewCustomer = true}, OrderStatus.Closed);
+        Add(new Input{ IsLargeOrder = true,IsRushOrder = true, OrderType = OrderType.Hire}, OrderStatus.Closed);
+        Add(new Input{ IsLargeOrder = true, OrderType = OrderType.Repair}, OrderStatus.AuthorisationRequired);
+        Add(new Input{ IsRushOrder = true, IsNewCustomer = true }, OrderStatus.AuthorisationRequired);
+    }
+}
