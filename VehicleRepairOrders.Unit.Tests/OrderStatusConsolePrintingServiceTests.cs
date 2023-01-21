ed782baf5d1a40ef4061f416a036294fb998ca0a
Tumelo Motsikelane <tumelo.motsikelane@gmail.com>
2023-01-21 21:03:17 +0200

    Added OrderStatusConsolePrintingServiceTests.cs

---
+++ VehicleRepairOrders.Unit.Tests/OrderStatusConsolePrintingServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace VehicleRepairOrders.Unit.Tests;
+
+public class OrderStatusConsolePrintingServiceTests
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Print_ShouldPrintTheOrderStatusAsAStringOnTheConsole(OrderStatus status, string expected)
+    {
+        var consoleWriteOutput = new StringWriter();
+        Console.SetOut(consoleWriteOutput);
+
+        var orderStatusConsolePrintingService = new OrderStatusConsolePrintingService();
+        orderStatusConsolePrintingService.Print(status);
+
+        consoleWriteOutput.ToString().Trim().Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { OrderStatus.Closed, OrderStatus.Closed.ToString() },
+            new object[] { OrderStatus.Confirmed, OrderStatus.Confirmed.ToString() },
+            new object[] { OrderStatus.AuthorisationRequired, OrderStatus.AuthorisationRequired.ToString() }
+        };
+}
+
