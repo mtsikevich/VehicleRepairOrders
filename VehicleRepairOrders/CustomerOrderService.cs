e446c1254fcc7a8985f1fdf401ffbc9be77ba3bb
Tumelo Motsikelane <tumelo.motsikelane@gmail.com>
2023-01-21 21:04:07 +0200

    Modified CustomerOrderService.cs implementation

--- VehicleRepairOrders/CustomerOrderService.cs
+++ VehicleRepairOrders/CustomerOrderService.cs
@@ -2,8 +2,21 @@
 
 public class CustomerOrderService: IOrderService
 {
+    private readonly Dictionary<Input, OrderStatus> _rules;
+
+    public CustomerOrderService()
+    {
+        _rules = new Dictionary<Input, OrderStatus>
+        {
+            {new Input{ IsLargeOrder = true, OrderType = OrderType.Repair, IsNewCustomer = true}, OrderStatus.Closed},
+            {new Input{ IsLargeOrder = true,IsRushOrder = true, OrderType = OrderType.Hire}, OrderStatus.Closed},
+            {new Input{ IsLargeOrder = true, OrderType = OrderType.Repair}, OrderStatus.AuthorisationRequired},
+            {new Input{ IsRushOrder = true, IsNewCustomer = true }, OrderStatus.AuthorisationRequired }
+        };
+    }
+    
     public OrderStatus GetOrderStatus(Input input)
     {
-        throw new NotImplementedException();
+        return _rules.ContainsKey(input) ? _rules[input] : OrderStatus.Confirmed;
     }
 }
